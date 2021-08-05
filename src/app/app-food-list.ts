e803d89fa6b5e2bfa3bc1f340bdd05bc3cfc8a62
unknown <mathi64it.com>
2021-08-05 15:37:33 +0530

    Adding the Project

---
+++ src/app/app-food-list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-grocery',
+	templateUrl: './app-food-list.html',
+	styleUrls: [
+        '../assets/css/bootstrap.min.css'
+    ]
+})
+
+export class GroceryComponent {
+
+  
+}
